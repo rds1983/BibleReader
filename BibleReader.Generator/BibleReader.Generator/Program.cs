1b3b0c0ab33158eaff4da6f5aa23211ef0f272fe
Roman Shapiro <eleroman@mail.ru>
2015-06-08 07:02:46 +0600

    Updated data for the june

--- BibleReader.Generator/BibleReader.Generator/Program.cs
+++ BibleReader.Generator/BibleReader.Generator/Program.cs
@@ -6,37 +6,32 @@ namespace BibleReader.Generator
 {
 	class MainClass
 	{
-		class Book
+		private class Book
 		{
-			public string Name { get; set; }
-			public int Chapters { get;set; }
-			public int StartMonth { get; set; }
-			public int StartDay { get;set; }
-			public int ChaptersPerDay { get;set; }
+			private readonly string _name;
+			private readonly int _chapters;
+			private readonly int _startMonth;
+			private readonly int _startDay;
+			private readonly int _chaptersPerDay;
 
-			public Book(string name, int chapters, int startMonth, int startDay, int chaptersPerDay)
+			public Book(string name, int chapters, int startMonth, int startDay, int chaptersPerDay = 1)
 			{
-				Name = name;
-				Chapters = chapters;
-				StartMonth = startMonth;
-				StartDay = startDay;
-				ChaptersPerDay = chaptersPerDay;
-			}
-
-			public Book(string name, int chapters, int startMonth, int startDay):
-				this(name, chapters, startMonth, startDay, 1)
-			{
+				_name = name;
+				_chapters = chapters;
+				_startMonth = startMonth;
+				_startDay = startDay;
+				_chaptersPerDay = chaptersPerDay;
 			}
 
 			public Reading[] CalculateReadings(DateTime dt)
 			{
-				var readingStart = new DateTime (dt.Year, StartMonth, StartDay);
+				var readingStart = new DateTime (dt.Year, _startMonth, _startDay);
 				if (dt < readingStart) {
 					return new Reading [0];
 				}
 
-				int readingDays = Chapters / ChaptersPerDay;
-				if (Chapters % ChaptersPerDay != 0) {
+				int readingDays = _chapters / _chaptersPerDay;
+				if (_chapters % _chaptersPerDay != 0) {
 					++readingDays;
 				}
 
@@ -46,14 +41,14 @@ namespace BibleReader.Generator
 					return new Reading [0];
 				}
 
-				var chaptersRead = ((int)(dt - readingStart).TotalDays * ChaptersPerDay);
-				var chaptersLeft = Chapters - chaptersRead;
-				var toRead = Math.Min (chaptersLeft, ChaptersPerDay);
+				var chaptersRead = ((int)(dt - readingStart).TotalDays * _chaptersPerDay);
+				var chaptersLeft = _chapters - chaptersRead;
+				var toRead = Math.Min (chaptersLeft, _chaptersPerDay);
 				var result = new List<Reading> ();
 
 				for(var i = chaptersRead + 1; i < chaptersRead + 1 + toRead; ++i) {
 					result.Add (new Reading {
-						BookName = Name,
+						BookName = _name,
 						Chapter = i
 					});
 				}
@@ -68,14 +63,20 @@ namespace BibleReader.Generator
 			public int Chapter {get;set;}
 		};
 
-		private static Book[] _books =new Book[]
+		private static readonly Book[] _books =new[]
 		{
 			new Book("Job", 42, 3, 22),
 			new Book("Solom", 19, 5, 3),
 			new Book("Sir", 52, 5, 22),
 			new Book("Is", 66, 4, 10, 2),
 			new Book("Jer", 52, 5, 13, 2),
+			new Book("Lam", 5, 6, 8, 2),
+			new Book("pJer", 1, 6, 10),
+			new Book("Bar", 5, 6, 11, 2),
+			new Book("Jer", 52, 6, 13, 2),
+
 			new Book("Mt", 28, 5, 21),
+			new Book("Mk", 16, 6, 18),
 			new Book("2Thes", 3, 3, 30),
 			new Book("1Tim", 6, 4, 2),
 			new Book("2Tim", 4, 4, 8),
@@ -85,7 +86,7 @@ namespace BibleReader.Generator
 			new Book("Apok", 22, 4, 29)
 		};
 
-		private static Reading[] CalculateReadingsForDate(int month, int day)
+		private static IEnumerable<Reading> CalculateReadingsForDate(int month, int day)
 		{
 			var year = DateTime.Now.Year;
 			var date = new DateTime (year, month, day);
